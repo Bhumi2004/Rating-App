41ddfca0d514bfa985100a31241458d095f7d932
Bhumi2004 <115335044+Bhumi2004@users.noreply.github.com>
2025-08-15 23:55:51 +0530

    Update ratings.js

--- backend/routes/ratings.js
+++ backend/routes/ratings.js
@@ -4,10 +4,10 @@ import { authMiddleware, roleMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-// Submit or update rating (normal users only)
+
 router.post("/", authMiddleware, roleMiddleware("user"), submitRating);
 
-// Get all ratings for a store (admin or owner)
+
 router.get("/:storeId", authMiddleware, roleMiddleware("admin", "owner"), getStoreRatings);
 
 export default router;
